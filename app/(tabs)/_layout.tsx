2b06b96a8033e7c58836cf452b5880b5b961ced3
Erzachel Senna Rizky Farezi <erzachelsrf@students.undip.ac.id>
2025-12-04 16:48:00 +0700

    update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,6 @@ import { Tabs } from "expo-router";
 export default function Layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
       <Tabs.Screen name="mahasiswa" options={{ title: "Mahasiswa" }} />
     </Tabs>
   );
--- src/firebase/firebaseAuth.ts
+++ src/firebase/firebaseAuth.ts
@@ -1,18 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { createMMKV } from 'react-native-mmkv';
 import { auth } from "./firebaseConfig";
-
-export const storage = createMMKV();
 
 export const login = async (email: string, password: string) => {
   const userCredential = await signInWithEmailAndPassword(auth, email, password);
-  storage.set("userUid", userCredential.user.uid);
-  storage.set("isLoggedIn", "1");
+  await AsyncStorage.setItem("userUid", userCredential.user.uid);
+  await AsyncStorage.setItem("isLoggedIn", "1");
   return userCredential.user;
 };
 
 export const logout = async () => {
   await signOut(auth);
-  storage.remove("userUid");
-  storage.remove("isLoggedIn");
+  await AsyncStorage.removeItem("userUid");
+  await AsyncStorage.removeItem("isLoggedIn");
 };
